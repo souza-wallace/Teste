a51624bb45a65db3ebc724037e938aef05808d5c
wallacesolutio <wallace.souza@solutiosoftware.com.br>
2022-01-29 16:15:38 -0300

    mudanca de conexao

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
  
 Class Connection{
  
-	private $server = "mysql:host=localhost;dbname=Teste";
-	private $username = "root";
-	private $password = "root";
+	private $server = "mysql:host=localhost;dbname=estudo";
+	private $username = "wallace";
+	private $password = "devsolutio";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  
